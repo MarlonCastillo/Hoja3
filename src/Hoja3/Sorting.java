c7dec0d0ddd23f4d02e3b89918aad8f5b31f3409
cristiandlcastillo <cristiandlcastillo@172.20.11.166>
2015-08-05 21:24:30 -0600

    Prueba

--- src/Hoja3/Sorting.java
+++ src/Hoja3/Sorting.java
@@ -15,6 +15,8 @@ public class Sorting {
 ALGORITMO DE SORTING
 este algoritmo fue tomado del siguiente enlace: 
 http://www.geekviewpoint.com/java/sorting/radixsort
+http://www.geekviewpoint.com/java/sorting/mergesort
+http://www.mycstutorials.com/articles/sorting/mergesort
 ----------------------------------------------------------------------------*/
     public static void radixSort(Datos[] input) {
 	  	final int RADIX = 10;
@@ -49,4 +51,90 @@ http://www.geekviewpoint.com/java/sorting/radixsort
                     placement *= RADIX;
 	  	}
 	}
+    public int [] mergesort(int input[]) {
+                if (input.length>1){
+                    int elementsInA1 = input.length/2;
+                    int elementsInA2 = input.length/2;
+                    if ((input.length % 2)==1){
+                        elementsInA2 += 1;   
+                    }
+                    int arr1[]=new int [elementsInA1];
+                    int arr2[]=new int [elementsInA1];
+                    for(int i = 0; i < elementsInA1; i++){
+			arr1[i] = input[i];
+                    }
+                    for(int i = elementsInA1; i < elementsInA1 + elementsInA2; i++){
+			arr2[i - elementsInA1] = input[i];
+                    }
+                    arr1 = mergesort (arr1);
+                    arr2 = mergesort (arr2);
+                    int i=0, j=0, k=0;
+                    while(arr1.length != j && arr2.length !=k){
+                        if (arr1[j] < arr2[k]){
+                            input[1] = arr1[j];
+                            i++;
+                            j++;
+                        }
+                        else{
+                            input[i]=arr2[k];
+                            i++;
+                            k++;
+                        }
+                    }
+                    while (arr1.length!=j){
+                        input[i]=arr1[j];
+                        i++;
+                        j++;
+                    }
+                    while (arr1.length!=j){
+                        input[i]=arr1[j];
+                        i++;
+                        k++;
+                    }
+                }
+            return input;
+        }
+    public static void mergesort(Datos [] input) {
+                if (input.length>1){
+                    int elementsInA1 = input.length/2;
+                    int elementsInA2 = input.length/2;
+                    if ((input.length % 2)==1){
+                        elementsInA2 += 1;   
+                    }
+                    Datos arr1[]=new Datos [elementsInA1];
+                    Datos arr2[]=new Datos [elementsInA1];
+                    for(int i = 0; i < elementsInA1; i++){
+                        arr1[i] = input[i];
+                    }
+                    for(int i = elementsInA1; i < elementsInA1 + elementsInA2; i++){
+                        arr2[i - elementsInA1] = input[i];
+                    }
+                    arr1 = mergesort (arr1);
+                    arr2 = mergesort (arr2);
+                    int i=0, j=0, k=0;
+                    while(arr1.length != j && arr2.length !=k){
+                        if (arr1[j] < arr2[k]){
+                            input[1] = arr1[j];
+                            i++;
+                            j++;
+                        }
+                        else{
+                            input[i]=arr2[k];
+                            i++;
+                            k++;
+                        }
+                    }
+                    while (arr1.length!=j){
+                        input[i]=arr1[j];
+                        i++;
+                        j++;
+                    }
+                    while (arr1.length!=j){
+                        input[i]=arr1[j];
+                        i++;
+                        k++;
+                    }
+                }
+        }
+    
 }
