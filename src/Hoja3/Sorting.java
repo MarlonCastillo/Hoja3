b69262f260f5e7c3ca99641e5740d9d1333661a6
Fernanda Dávila <fernandadavila@otroround.com>
2015-08-05 22:45:01 -0600

    Aún no he hecho muchas modificaciones.

--- src/Hoja3/Sorting.java
+++ src/Hoja3/Sorting.java
@@ -1,16 +1,9 @@
 package Hoja3;
 
-/**
- *
- * @author Marlon
- */
-
 //importamos las clases a utilizar
 import java.util.List;
 import java.util.ArrayList;
 
-public class Sorting {
-    
 /*--------------------------------------------------------------------------
 ALGORITMO DE SORTING
 este algoritmo fue tomado del siguiente enlace: 
@@ -18,39 +11,42 @@ http://www.geekviewpoint.com/java/sorting/radixsort
 http://www.geekviewpoint.com/java/sorting/mergesort
 http://www.mycstutorials.com/articles/sorting/mergesort
 ----------------------------------------------------------------------------*/
+
+public class Sorting {
     public static void radixSort(Datos[] input) {
-	  	final int RADIX = 10;
-	  	// declare and initialize bucket[]
-	  	List<Datos>[] bucket = new ArrayList[RADIX];
-	  	for (int i = 0; i < bucket.length; i++) {
-                    bucket[i] = new ArrayList<Datos>();
-	  	}
-	 
-	  	// sort
-	  	boolean maxLength = false;
-	  	int tmp = -1, placement = 1;
-	  	while (!maxLength) {
-                    maxLength = true;
-                    // split input between lists
-                    for (Datos i : input) {
-                            tmp = i.getDato() / placement;
-                            bucket[tmp % RADIX].add(i);
-                            if (maxLength && tmp > 0) {
-                                    maxLength = false;
-                            }
-                    }
-                    // empty lists into input array
-                    int a = 0;
-                    for (int b = 0; b < RADIX; b++) {
-                            for (Datos i : bucket[b]) {
-                                    input[a++] = i;
-                            }
-                            bucket[b].clear();
-                    }
-                    // move to next digit
-                    placement *= RADIX;
-	  	}
-	}
+        final int RADIX = 10;
+        // declare and initialize bucket[]
+        List<Datos>[] bucket = new ArrayList[RADIX];
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = new ArrayList<Datos>();
+        }
+
+        // sort
+        boolean maxLength = false;
+        int tmp = -1, placement = 1;
+        while (!maxLength) {
+        maxLength = true;
+        // split input between lists
+            for (Datos i : input) {
+            tmp = i.getDato() / placement;
+            bucket[tmp % RADIX].add(i);
+                if (maxLength && tmp > 0) {
+                        maxLength = false;
+                }
+            }
+            // empty lists into input array
+        int a = 0;
+        for (int b = 0; b < RADIX; b++) {
+            for (Datos i : bucket[b]) {
+                input[a++] = i;
+            }
+        bucket[b].clear();
+        }
+
+        // move to next digit
+        placement *= RADIX;
+        }
+    }
     public static Datos [] mergesort(Datos input[]) {
                 if (input.length>1){
                     int elementsInA1 = input.length/2;
